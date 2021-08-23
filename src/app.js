1a5a8e73dcbd1891cfda8a5ed19111a7f209865b
luigifurlani13 <luis.furlan@icloud.com>
2021-08-23 18:45:10 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -8,4 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  randomWebsite();
 };
+
+let characters = ["Meliodas", "Inuyasha", "Vegeta", "Yugi", "Eren"];
+
+let adjectives = ["Wrathful", "Halfblood", "Restless", "Legendary", "Valient"];
+
+let noun = ["Captain", "Demon", "Prince", "Pharaoh", "Titan"];
+
+var lastItem = characters.length - 1;
+
+var selected = Math.floor(Math.random() * lastItem);
+var chosen = Math.floor(Math.random() * lastItem);
+var highlighted = Math.floor(Math.random() * lastItem);
+
+function randomWebsite() {
+  let website =
+    characters[selected] + adjectives[chosen] + noun[highlighted] + ".com";
+  document.querySelector("#website").innerHTML = website;
+}
